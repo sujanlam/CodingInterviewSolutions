07bbeaa36c0ff035bd35328f684ac8380c01f884
Sujan Lamichhane <106173760+sujanlam@users.noreply.github.com>
2024-10-03 20:21:11 -0500

    Add files via upload

---
+++ BackSpaceStringCompare.java
@@ -0,0 +1,38 @@
+package strings;
+
+import java.util.Stack;
+
+public class BackSpaceStringCompare {
+    public static void main(String[] args) {
+        String s1 = "axx#bb#c";
+        String s2 = "axbd#c#c";
+        System.out.println(areStringsEqualWithBackSpaces(s1, s2));
+    }
+
+    private static int areStringsEqualWithBackSpaces(String s1, String s2) {
+
+        Stack<Character> stack1 = new Stack<Character>();
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            if(!stack1.isEmpty() && c =='#') {
+                stack1.pop();
+            }
+            if(c != '#') stack1.push(c);
+        }
+        System.out.println(stack1);
+        Stack<Character> stack2 = new Stack<Character>();
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            if(!stack2.isEmpty() && c =='#') {
+                stack2.pop();
+            }
+            if(c != '#') stack2.push(c);
+        }
+        System.out.println(stack2);
+        while(!stack1.isEmpty() && !stack2.isEmpty()) {
+            if(stack1.pop() != stack2.pop()) return 0;
+        }
+
+        return 1;
+    }
+}
